2c4f72dada62a83790ff62d05ef24852fb79fc8f
Jop <jopmolenaar@icloud.com>
2024-04-02 12:31:05 +0200

    reorganized files

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import { App } from '@tinyhttp/app';
 import { logger } from '@tinyhttp/logger';
 import { Liquid } from 'liquidjs';
 import sirv from 'sirv';
-import fs from 'node:fs';
+// import fs from 'node:fs';
 
 const engine = new Liquid({
   extname: '.liquid'
@@ -14,25 +14,25 @@ const app = new App();
 app.use(logger()).use("/", sirv("dist/assets")).listen(3080);
 
 app.get('/', async (req, res) => {
-  // const movieData = await getMovies();
-  return res.send(renderTemplate("views/index.liquid"));
-  // return res.send(renderTemplate('views/index.liquid', { title: 'Movies', movieData }));
+  const movieData = await getMovies();
+  // console.log(movieData);
+  // return res.send(renderTemplate("views/index.liquid"));
+  return res.send(renderTemplate("src/views/index.liquid", { title: "Movies", movieData }));
   // return res.send(renderTemplate('views/index.liquid', { title: 'Home' }));
 });
 
-app.get('/movie/:id/', async (req, res) => {
-  const movieId = req.params.id;
-  const movie = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.MOVIEDB_TOKEN}`).then(res => res.json());
-  return res.send(renderTemplate('views/detail.liquid', { title: 'Movie', movie }));
+app.get("/movie/:id/", async (req, res) => {
+    // return res.send(renderTemplate("views/detail.liquid"));
+    const movieId = req.params.id;
+    const movie = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.MOVIEDB_TOKEN}`).then((res) => res.json());
+    return res.send(renderTemplate("src/views/detail.liquid", { title: "Movie", movie }));
 });
 
 const getMovies = async () => {
-  const response = await fetch(`https://api.themoviedb.org/3/trending/all/day?language=en-US&api_key=${process.env.MOVIEDB_TOKEN}`);
-  const movieData = await response.json();
-
-
-  console.log('movieData', movieData);
-  return movieData;
+    const response = await fetch(`https://api.themoviedb.org/3/trending/all/day?language=en-US&api_key=${process.env.MOVIEDB_TOKEN}`);
+    const movieData = await response.json();
+    // console.log("movieData", movieData);
+    return movieData;
 };
 
 
