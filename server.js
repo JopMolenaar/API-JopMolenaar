eb9f969abfb9b2b98b735506145b8241847fcdb5
Jop <jopmolenaar@icloud.com>
2024-04-02 15:09:51 +0200

    tinyhttp to express and working chat rn

--- server.js
+++ server.js
@@ -1,47 +1,88 @@
-import 'dotenv/config';
-import { App } from '@tinyhttp/app';
-import { logger } from '@tinyhttp/logger';
-import { Liquid } from 'liquidjs';
-import sirv from 'sirv';
-// import fs from 'node:fs';
+require("dotenv").config();
+const express = require("express");
+const { Liquid } = require("liquidjs");
+const sirv = require("sirv");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const engine = new Liquid({
-  extname: '.liquid'
+    extname: ".liquid",
 });
 
-const app = new App();
+const app = express();
 
-app.use(logger()).use("/", sirv("dist/assets")).listen(3080);
+app.use(sirv("dist/assets"));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', async (req, res) => {
-  const movieData = await getMovies();
-  // console.log(movieData);
-  // return res.send(renderTemplate("views/index.liquid"));
-  return res.send(renderTemplate("src/views/index.liquid", { title: "Movies", movieData }));
-  // return res.send(renderTemplate('views/index.liquid', { title: 'Home' }));
+app.get("/status", (request, response) => response.json({ clients: clients.length }));
+
+const PORT = process.env.PORT || 4000;
+
+let clients = [];
+let facts = [];
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
 });
 
-app.get("/movie/:id/", async (req, res) => {
-    // return res.send(renderTemplate("views/detail.liquid"));
-    const movieId = req.params.id;
-    const movie = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.MOVIEDB_TOKEN}`).then((res) => res.json());
-    return res.send(renderTemplate("src/views/detail.liquid", { title: "Movie", movie }));
+app.get("/chat", async (req, res) => {
+    return res.send(renderTemplate("src/views/detail.liquid"));
 });
-
-const getMovies = async () => {
-    const response = await fetch(`https://api.themoviedb.org/3/trending/all/day?language=en-US&api_key=${process.env.MOVIEDB_TOKEN}`);
-    const movieData = await response.json();
-    // console.log("movieData", movieData);
-    return movieData;
-};
-
 
 const renderTemplate = (template, data) => {
-  const templateData = {
-    NODE_ENV: process.env.NODE_ENV || 'production',
-    ...data
-  };
+    const templateData = {
+        NODE_ENV: process.env.NODE_ENV || "production",
+        ...data,
+    };
 
-  return engine.renderFileSync(template, templateData);
+    return engine.renderFileSync(template, templateData);
 };
 
+function eventsHandler(request, response, next) {
+    const headers = {
+        "Content-Type": "text/event-stream",
+        Connection: "keep-alive",
+        "Cache-Control": "no-cache",
+    };
+    response.writeHead(200, headers);
+
+    const data = `data: ${JSON.stringify(facts)}\n\n`;
+
+    response.write(data);
+
+    const clientId = Date.now();
+    console.log(`${clientId} Connection opened`);
+    const newClient = {
+        id: clientId,
+        response,
+    };
+
+    clients.push(newClient);
+
+    request.on("close", () => {
+        console.log(`${clientId} Connection closed`);
+        clients = clients.filter((client) => client.id !== clientId);
+    });
+}
+
+function sendEventsToAll(newFact) {
+    console.log(clients);
+    clients.forEach((client) => client.response.write(`data: ${client.id} ${JSON.stringify(newFact)}\n\n`));
+}
+
+async function addFact(request, response, next) {
+    const newFact = request.body;
+    console.log(newFact);
+    facts.push(newFact);
+    response.json(newFact);
+    return sendEventsToAll(newFact);
+}
+
+app.get("/events", eventsHandler);
+app.post("/fact", addFact);
+
+// import { json, urlencoded } from "body-parser";
+// import { cors } from "@tinyhttp/cors";
+// issues with this when writing it with tinyhttp
